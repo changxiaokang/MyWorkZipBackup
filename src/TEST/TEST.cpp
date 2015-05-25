a70ef23cdd103132f5b97988284f5c9420590208
xk <changxiaokang@hotmail.com>
2015-05-25 19:05:33 +0800

    update

--- src/TEST/TEST.cpp
+++ src/TEST/TEST.cpp
@@ -1,33 +1,64 @@
 #include <stdafx.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <windows.h>
 
-void EncryptFun();
+/*
+  ary表示数组第0个元素地址的指针常量，指针类型为元素类型
+  type ary[M];
+
+  ary[n] <==> *(type*)((int)ary + sizeof(type)*n)
+  type *prt = xxxx;
+  type *ptrTest = xxxx;
+  int n = xxxx;
+
+  (ptr + n) is (int)ptr + sizeof(type)*n;
+  ptr + n <==> (type* const)((int)ptr + sizeof(type)*n);
+  ptr - ptrTest <==> ((int)ptr - (int)ptrTest) / sizeof(type)
+
+  ary[n] == *(ary + n);
+  &ary[n] == ary + n;
+
+  type *ptr = ary;
+  ptr[n] == ary[n];
+  ptr++; // ok
+  ary++; // err, ary is pointer const
+*/
+
+void exchange(int* pa, int* pb);
 
 int main()
 {
-  DWORD dwOld;
-  VirtualProtect(EncryptFun, 0x1, PAGE_EXECUTE_READWRITE, &dwOld);
-  char* PFNPROC = (char*)EncryptFun;
+  int ary[5] = 
+  {
+    1, 2, 3, 4, 5
+  };
 
-#ifdef _DEBUG
-  PFNPROC = (char*)((int)EncryptFun + *(int*)(PFNPROC + 1) + 5);
-#endif
-  // *PFNPROC = '\0xCC';
-  EncryptFun();
+  int* pAry = ary;
+  int i = 3;
+
+  printf("&pAry[3] = %p \t &ary[3] = %p\r\n", &pAry[3], &ary[3]);
+  printf("pAry + 3 = %p \t ary + 3 = %p\r\n", pAry + 3, ary + 3);
+
+  if(*ary == *pAry)
+    puts("*ary == *pAry");
+
+  pAry++;
+  printf("%d\r\n", pAry - ary);
+
+  int a = 2; 
+  int b = 8;
+
+  int* pa = &a;
+  int* pb = &b;
+  exchange(pa, pb);
+
+  printf("a = %d, b = %d\r\n", a, b);
 
   return 0; 
 }
 
-void EncryptFun()
+void exchange(int* pa, int* pb)
 {
-  int nRandom = 1;
-  srand((unsigned)time(NULL));
-  for(int i = 0; i < 10; i++)
-  {
-    nRandom = rand() % 11 + 10;
-    printf("%d\r\n", nRandom);
-  }
+  int nTemp = *pa;
+  *pa = *pb;
+  *pb = nTemp;
 }
