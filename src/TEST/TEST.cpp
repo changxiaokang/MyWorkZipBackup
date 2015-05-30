c219dfe6547c5aba768ac1c31d2a9a84ea618bed
xk <changxiaokang@hotmail.com>
2015-05-30 11:37:32 +0800

    update

--- src/TEST/TEST.cpp
+++ src/TEST/TEST.cpp
@@ -1,59 +1,24 @@
 #include <stdafx.h>
 #include <stdio.h>
 
-int PartionSort(int nArray[], int nStart, int nEnd);
-void  QuickSort(int nArray[], int nStart, int nEnd);
+#define A 97
+#define Z 121
 
 int main()
 {
-  int nTemp[5] = {0};
-  int nArray[] = {2, 5, 4, 6, 0, 1, 3, 7, 2};
-  int nCount = sizeof(nArray)/sizeof(int);
-  
-  QuickSort(nArray, 0, nCount-1);
-  
-  for(int i = 0; i < nCount; i++)
-    printf("%d\r\n", nArray[i]);
-  
-  return 0;
-}
-
-int PartionSort(int nArray[], int nStart, int nEnd)
-{
-  int i = 0;
-  int j = 0;
-  int nTemp = 0;
-  int nMiddle = 0;
-  nMiddle = nArray[nEnd];
-  
-  if(nStart < nEnd)
+  for(int i = A; i < Z; i++)
   {
-    while(j < nEnd)
-    { 
-      if(nArray[j] <= nMiddle)
+    for(int j = A; j < Z; j++)
+    {
+      for(int k = A; k < Z; k++)
       {
-        nTemp = nArray[i];
-        nArray[i] = nArray[j];
-        nArray[j] = nTemp;
-        i++;
+        if(i == 's')
+        {
+          printf("it%c%c%c\t", i, j, k);
+        }
       }
-      j++;
     }
-    nTemp = nArray[i];
-    nArray[nEnd] = nTemp;
-    nArray[i] = nArray[nEnd];
   }
-  
-  return i;
-}
 
-void QuickSort(int nArray[], int nStart, int nEnd)
-{
-  int nIndex = 0;
-  if(nStart < nEnd)
-  {
-    nIndex = PartionSort(nArray, nStart, nEnd);
-    QuickSort(nArray, nStart, nIndex-1);
-    QuickSort(nArray, nIndex+1, nEnd);
-  }
+  return 0;
 }
