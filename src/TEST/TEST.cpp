9d7ddb0f671f2f0801f87902be11f3b258ca2b92
xk <changxiaokang@hotmail.com>
2015-06-14 23:01:48 +0800

    update

--- src/TEST/TEST.cpp
+++ src/TEST/TEST.cpp
@@ -1,24 +1,18 @@
 #include <stdafx.h>
 #include <stdio.h>
 
-#define A 97
-#define Z 121
+int MyStrLen(const char* str)
+{
+  const char* eos = str;
+
+  while(*eos++) ;
+
+  return ((int)(eos - str - 1));
+}
 
 int main()
 {
-  for(int i = A; i < Z; i++)
-  {
-    for(int j = A; j < Z; j++)
-    {
-      for(int k = A; k < Z; k++)
-      {
-        if(i == 'm')
-        {
-          printf("i%c%c%c\t", i, j, k);
-        }
-      }
-    }
-  }
+  MyStrLen("changxiaokang");
 
   return 0;
 }
