b96fd9ab8a07bb281b9d73a08e7407b24f3d28a6
xk <changxiaokang@hotmail.com>
2015-07-01 00:00:35 +0800

    update

--- src/TEST/TEST.cpp
+++ src/TEST/TEST.cpp
@@ -1,18 +1,55 @@
 #include <stdafx.h>
 #include <stdio.h>
 
-int MyStrLen(const char* str)
+void Add()
 {
-  const char* eos = str;
-
-  while(*eos++) ;
-
-  return ((int)(eos - str - 1));
+  puts("Add");
 }
+void Del()
+{
+  puts("Del");
+}
+void Set()
+{
+  puts("Set");
+}
+void Find()
+{
+  puts("Find");
+}
+
+typedef void (*PFNPROC)(); // void (*g_fnAry[4])() = {}
+PFNPROC g_fnAry[4] = 
+{
+  Add,
+  Del,
+  Set,
+  Find
+};
+
+void InitAry(/*void (*pfnAry[])()*/PFNPROC pfnAry[], int nCount)
+{
+  int nId;
+  puts("Make you choice 1:Add;2:Del;3:Set;4:Find");
+  for(int i = 0; i < 4; i++)
+  {
+    scanf("%d", &nId);
+    pfnAry[i] = g_fnAry[nId-1];
+  }
+};
 
 int main()
 {
-  MyStrLen("changxiaokang");
-
-  return 0;
+  // void (*PFNaRY[4])() = {NULL};
+  PFNPROC pfnAry[4] = {0};
+  
+  InitAry(pfnAry, sizeof(pfnAry) / sizeof(pfnAry[0]));
+  
+  for(int i = 0; i < sizeof(pfnAry) / sizeof(pfnAry[0]); i++)
+  {
+    pfnAry[i]();
+  }
+  
+  return 0; 
 }
+
