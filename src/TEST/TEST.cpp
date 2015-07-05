6e746d930fb28206dabba69b49e2594f02f3fcdb
xk <changxiaokang@hotmail.com>
2015-07-06 00:08:23 +0800

    update

--- src/TEST/TEST.cpp
+++ src/TEST/TEST.cpp
@@ -1,55 +1,20 @@
 #include <stdafx.h>
-#include <stdio.h>
+#include "boost/thread.hpp"
+#include "iostream"
 
-void Add()
-{
-  puts("Add");
-}
-void Del()
-{
-  puts("Del");
-}
-void Set()
-{
-  puts("Set");
-}
-void Find()
-{
-  puts("Find");
-}
+using namespace std;
 
-typedef void (*PFNPROC)(); // void (*g_fnAry[4])() = {}
-PFNPROC g_fnAry[4] = 
+void mythread()
 {
-  Add,
-  Del,
-  Set,
-  Find
-};
-
-void InitAry(/*void (*pfnAry[])()*/PFNPROC pfnAry[], int nCount)
-{
-  int nId;
-  puts("Make you choice 1:Add;2:Del;3:Set;4:Find");
-  for(int i = 0; i < 4; i++)
-  {
-    scanf("%d", &nId);
-    pfnAry[i] = g_fnAry[nId-1];
-  }
-};
+  cout << " hello,thread! " << endl;
+}
 
 int main()
 {
-  // void (*PFNaRY[4])() = {NULL};
-  PFNPROC pfnAry[4] = {0};
+  boost::function<void()> f(mythread);
+  boost::thread t(f);
+  t.join();
+  cout << " thread is over! " << endl;
   
-  InitAry(pfnAry, sizeof(pfnAry) / sizeof(pfnAry[0]));
-  
-  for(int i = 0; i < sizeof(pfnAry) / sizeof(pfnAry[0]); i++)
-  {
-    pfnAry[i]();
-  }
-  
-  return 0; 
+  return 0;
 }
-
