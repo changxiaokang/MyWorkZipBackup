453039f5ce152799c70df677d8d90184d68eae1c
xk <changxiaokang@hotmail.com>
2015-08-14 18:26:06 +0800

    update

--- src/TEST/TEST.c
+++ src/TEST/TEST.c
@@ -1,7 +1,34 @@
 #include <stdio.h>
 
+struct tagGoogs
+{
+	char szName[20];
+	float fPrice;
+	int nNum;
+	float fMoney;
+};
+
+void Calculate(struct tagGoogs* a, int nX);
+void Sort(struct tagGoogs* a, int nX);
+void OutPut(struct tagGoogs* a, int nX);
+
 int main(void)
 {
+	struct tagGoogs stu[4] = 
+	{
+		{"XXX", 0.1f, 5, 0.2f}, 
+		{"XXX", 0.1f, 5, 0.2f},
+		{"XXX", 0.1f, 5, 0.2f},
+		{"XXX", 0.1f, 5, 0.2f}
+	};
+
+	Calculate(stu, 4);
+	Sort(stu, 4);
+	OutPut(stu, 4);
 
 	return 0;
 }
+
+void Calculate(struct tagGoogs* a, int nX) {}
+void Sort(struct tagGoogs* a, int nX) {}
+void OutPut(struct tagGoogs* a, int nX) {}
